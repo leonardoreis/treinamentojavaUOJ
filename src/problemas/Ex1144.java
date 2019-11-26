0bd629f96205ceebffcca43b8690b01ebad71e06
leonardoreis <leonardo.demian@gmail.com>
2019-11-26 14:53:33 -0300

    Ex1144 finalizado

--- src/problemas/Ex1144.java
+++ src/problemas/Ex1144.java
@@ -10,24 +10,29 @@ public class Ex1144 {
 		
 		int lacoj = 0;
 		int testej = 1;
-		boolean par = true;
+		boolean intercalar = true;
+		int lacok=6;
+		int testek=1;
+		int somak=6;
 		
-		//System.out.println("1 1 1");
 		for(double i = 1; i < entrada+1; i=i+0.5) {
-			for (int j = 0; j < 1; j++) {
-				if (par==true) {
-					System.out.println((int)i+" "+((int)i+testej));
-					par=false;
-				}else { 
-					lacoj = lacoj+2;
-					System.out.println((int)i+" "+((int)i+lacoj));
-					par=true;
-				}
+				System.out.println((int)i +" "+ (testej)+" "+testek);
 				
+				for (int j = 0; j < 1; j++) {
+					if (intercalar) {
+						intercalar=false;
+						testej++;
+						testek++;
+					}else {
+						intercalar=true;
+						lacoj = lacoj+2;
+						testej = testej+lacoj;
+						testek = testek+somak;
+						lacok= lacok+6;
+						somak = somak+lacok;
+					}
 			}
-				
 		}
-		
-		
+			
 	}//fim do metodo main
 }//fim da classe
